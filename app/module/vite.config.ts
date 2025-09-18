12e13186d0a40372729af89f77abf2ab22fbcad0
Gary Rutland <gary.rutland@dae.mn>
2025-09-18 16:53:44 +0100

    Turned off tabs

--- app/module/vite.config.ts
+++ app/module/vite.config.ts
@@ -2,10 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		host: true,
-		allowedHosts: ['localhost', 'core'],
-		port: 3000
-	}
+  plugins: [sveltekit()],
+  server: {
+    host: true,
+    allowedHosts: ['localhost', 'core'],
+    port: 3000
+  }
 });
